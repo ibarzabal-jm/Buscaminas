7c8bd7c3bb25c332db994d4b65d1995942efb8e9
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-05-29 04:41:17 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,48 +1,18 @@
-var contenedorTablero = document.getElementById("tablero");
+const FILAS = 10;
+const COLUMNAS = 10;
 
 
 
-var matriz = new Array(3);
+let contenedorTablero = document.getElementById("tablero");
 
-for ( i = 0; i< matriz.length; i ++){
-    matriz[i] = new Array(3);
-}
-
-matriz[0][0] = 1;
-matriz[0][1] = 2;
-matriz[0][2] = 3;
-
-matriz[1][0] = 4;
-matriz[1][1] = 5;
-matriz[1][2] = 6;
-
-matriz[2][0] = 7;
-matriz[2][1] = 8;
-matriz[2][2] = 9;
-
-
-for (i = 0; i < matriz.length; i++)
-{
-    for (var j = 0; j < matriz[i].length; j++)
-    {
-        contenedorTablero.innerHTML += (matriz[i][j]);
-    }
-
-    contenedorTablero.innerHTML +=`<br/>`;
-}
-
-/*
 crearTablero();
 
 function crearTablero(){
 
-    tablero.innerHTML="";
     for(let i = 0 ; i < 10 ; i++){
-        let row = tablero.insertRow(i);
-        for( let j = 0; j < 10 ; j++){
-            celda = row.insertCelda(j);
-        }
+
     }
+    
 }
 
-*/
+
