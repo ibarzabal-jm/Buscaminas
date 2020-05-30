f3f9cb91f54df9fabc31d5ebb0cc719961347ff2
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-05-30 01:36:46 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,46 @@
 const FILAS = 10;
 const COLUMNAS = 10;
 
-
-
 let contenedorTablero = document.getElementById("tablero");
+contenedorTablero.innerHTML = ''
 
-crearTablero();
+
+
 
 function crearTablero(){
-
-    for(let i = 0 ; i < 10 ; i++){
-
-    }
+    tablero= new Array(FILAS);
     
+    for ( var i = 0; i< tablero.length; i ++){
+        tablero[i] = new Array(COLUMNAS);
+        for ( var j = 0; j< tablero[i].length; j++){
+            tablero[i][j] = 0;
+        }
+    }
+
+    return tablero;
+
 }
 
+tablero = crearTablero();
+
+mostrarTablero(tablero);
+
+function mostrarTablero(tablero){
+    
+    contenedorTablero.innerHTML = '';
+
+    for (i = 0; i < tablero.length; i++){
+
+        contenedorTablero.innerHTML += `<tr>`;
+
+        
+        for (var j = 0; j < tablero[i].length; j++)
+        {
+            contenedorTablero.innerHTML += `<td>${j}</td>`;          
+        }
+
+        contenedorTablero.innerHTML += `</tr>`;
+
+    }
+}
 
